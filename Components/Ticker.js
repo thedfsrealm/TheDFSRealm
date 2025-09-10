7c3ce23182b04804076606b6a1af1c3f54310eca
Stephen Williams <topknox@gmail.com>
2025-09-09 23:11:04 -0500

    Delete Components/Ticker.js

--- Components/Ticker.js
+++
@@ -1,9 +0,0 @@
-export default function Ticker() {
-  return (
-    <div className="bg-black text-green-400 py-2 px-4 overflow-hidden whitespace-nowrap">
-      <marquee behavior="scroll" direction="left">
-        DFS Realm Alive · Spirit-led Wisdom · Lineups Breathing · Caretaker Blessings Flowing
-      </marquee>
-    </div>
-  );
-}
