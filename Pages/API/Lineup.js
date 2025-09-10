b8f28b466b693479f64ad15ca6f2fe6357c0c566
Stephen Williams <topknox@gmail.com>
2025-09-09 23:16:14 -0500

    Delete Pages/API/Lineup.js

--- Pages/API/Lineup.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const lineup = {
-    captain: "Justin Jefferson",
-    flex: ["JJ McCarthy", "D’Andre Swift", "T.J. Hockenson", "Adam Thielen", "Kyle Monangai"],
-    salaryUsed: 49700,
-    spiritBlessing: "Abundance, peace, renewal are yours."
-  };
-  res.status(200).json({ lineup });
-}
